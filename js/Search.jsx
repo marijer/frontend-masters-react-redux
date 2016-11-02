244ebe9e1481e7fe483586beb77b98c4c0db8597
Rooze <marije.rooze@kro-ncrv.nl>
2016-11-02 11:26:40 +0100

    update search component with filter function

--- js/Search.jsx
+++ js/Search.jsx
@@ -2,14 +2,42 @@ const React = require('react');
 const data = require('../public/data');
 const ShowCard = require('./ShowCard');
 
-const Search = () => (
-  <div className='container'>
-    <div className='shows'>
-      {data.shows.map((show) => {
-        return <ShowCard {...show} key={show.imdbID} />
-      })}
+class Search extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      searchTerm: ''
+    };
+
+    this.handleSearchTermEvent = this.handleSearchTermEvent.bind(this);
+  }
+
+  handleSearchTermEvent (event) {
+    this.setState({searchTerm: event.target.value });
+  }
+
+  render () {
+      return (
+        <div className='container'>
+      <header className='header'>
+        <h1 className='brand'>svideo</h1>
+          <input value={this.state.searchTerm}
+                className='search-input'
+                type='text'
+                onChange={this.handleSearchTermEvent}
+                placeholder='Search' />
+        </header>
+      <div className='shows'>
+        {data.shows
+          .filter((show) => `${show.title} ${show.description}`.toUpperCase()
+              .indexOf(this.state.searchTerm.toUpperCase()) >= 0)
+          .map((show) => {
+          return <ShowCard {...show} key={show.imdbID} />
+        })}
+      </div>
     </div>
-  </div>
-);
+  )}
+};
 
 module.exports = Search;
